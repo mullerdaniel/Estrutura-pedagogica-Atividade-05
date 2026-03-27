b0820e59a6cfb2100971e3c910d83db06ab79750
Daniel Müller <miladaniel370@gmail.com>
2026-03-27 18:40:17 -0300

    Feat: Adicionando estrutura base springboot

---
+++ src/main/java/com/weg/estrutura/pedagogica/EstruturaPedagogicaApplication.java
@@ -0,0 +1,13 @@
+package com.weg.estrutura.pedagogica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstruturaPedagogicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstruturaPedagogicaApplication.class, args);
+	}
+
+}
